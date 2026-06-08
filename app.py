77ee4b32eb8e431b6327b339f6a889875c9ace40
elkadirist <elkadirist@hotmail.com>
2026-06-09 01:52:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ st.set_page_config(page_title="PRO AI TRADING DASHBOARD", layout="wide")
 # =========================
 stocks = [
     "THYAO.IS","ASELS.IS","BIMAS.IS","FROTO.IS",
-    "SISE.IS","KOZAL.IS","ASTOR.IS","SASA.IS",
-    "EKGYO.IS","GARAN.IS","YKBNK.IS"
+    "SISE.IS","FRIGO.IS","ASTOR.IS","SASA.IS",
+    "PGSUS.IS","RNPOL.IS","ATATR.IS","TEKTU.IS","GWIND.IS"
 ]
 
 # =========================
